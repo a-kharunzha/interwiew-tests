a32536163ac5ff4fa5dfa53a8d74e8565fa95742
Alex Kharunzha <rolland.archer@gmail.com>
2019-03-07 08:49:47 +0200

    Описание структуры и команд, файл сертификата сделан опциональным

--- tutu-ru/solutions/country_bot/cli-commands/SetWebhookCommand.php
+++ tutu-ru/solutions/country_bot/cli-commands/SetWebhookCommand.php
@@ -2,11 +2,9 @@
 
 namespace TuTuRu\Command;
 
-use Longman\TelegramBot\Exception\TelegramException;
 use Longman\TelegramBot\Telegram;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-// use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 
 // load environment
@@ -37,18 +35,45 @@ class SetWebhookCommand extends Command
             $telegram = new Telegram($bot_api_key, $bot_username);
 
             // Set webhook
+            $options = $this->makeOptions($output);
             $result = $telegram->setWebhook(
                 $hook_url,
-                [
-                    'certificate' => $_SERVER['DOCUMENT_ROOT'].getenv('TELEGRAM_BOT_CERT')
-                ]
+                $options
             );
             if ($result->isOk()) {
                 $output->writeln('OK: ' . $result->getDescription());
             }
-        } catch (TelegramException $e) {
+        } catch (\Exception $e) {
             // log telegram errors
             $output->writeln('Error: ' . $e->getMessage());
         }
+    }
+
+    /**
+     * @param $output
+     *
+     * @return array
+     * @throws \Exception
+     */
+    private function makeOptions($output): array{
+        $options = [];
+        if(
+        $certPath = getenv('TELEGRAM_BOT_CERT')
+        ){
+            $output->writeln('Use certificate: ' . $certPath);
+            $certFullPath = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.$certPath;
+            if(
+                file_exists($certFullPath )
+                &&
+                is_file($certFullPath )
+            ){
+                $options ['certificate'] = $certFullPath ;
+            }else{
+                throw new \Exception('Cert file set in .env but not found: '.$certFullPath);
+            }
+        }else{
+            $output->writeln('Running without attached certificate');
+        }
+        return $options;
     }
 }
